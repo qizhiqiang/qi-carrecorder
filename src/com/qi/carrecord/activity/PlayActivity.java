bc375f5785bf8d1ca8f868bb93af985dbcd90d86
qizhiqiang <qizhiqiang2008@163.com>
2015-10-17 16:20:19 +0800

    basic func

--- src/com/qi/carrecord/activity/PlayActivity.java
+++
@@ -1,7 +0,0 @@
-package com.qi.carrecord.activity;
-
-import android.app.Activity;
-
-public class PlayActivity extends Activity {
-
-}
--- src/com/qi/carrecord/activity/SettingActivity.java
+++
@@ -1,7 +0,0 @@
-package com.qi.carrecord.activity;
-
-import android.app.Activity;
-
-public class SettingActivity extends Activity {
-
-}
